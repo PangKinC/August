db220e87ec3f6cc47e953a4a1c8d126c0728228c
PangKinC <kinchungpang@gmail.com>
2016-08-10 23:26:41 +0100

    strings

--- quiz.js
+++ quiz.js
@@ -11,17 +11,21 @@ function Question(questionText, answerOne, answerTwo, answerThree, answerFour, c
 
 var questionOne = new Question('Which of these is not a handheld console?', 'Playstation Portable', 'Nintendo DS', 'Nintendo Wii', 'Game Boy Advance', 3);
 var questionTwo = new Question('Which of these is not part of the original 151 Pokemon?', 'Kangaskhan', 'Bulbasaur', 'Pikachu', 'Suicune', 4);
-var questionThree = new Question('Which of these series is not a franchise belonging to Nintendo?', 'Pokemon', 'Final Fantasy', 'Legend of Zelda', 'Metroid', 2)
+var questionThree = new Question('Which of these series is not a franchise belonging to Nintendo?', 'Pokemon', 'Final Fantasy', 'Legend of Zelda', 'Metroid', 2);
+var questionFour = new Question('Which of the following Final Fantasy did not launch on the Super Nintendo?', 'Final Fantasy III', 'Final Fantasy IV', 'Final Fantasy V', 'Final Fantasy VI', 1);
+var questionFive = new Question('Which of the following Legend of Zelda titles is on a handheld system?', 'Ocarina of Time', 'Twilight Princess', 'Minish Cap', 'Wind Waker', 3);
 
 questionList = []
 
 questionList.push(questionOne);
 questionList.push(questionTwo);
 questionList.push(questionThree);
+questionList.push(questionFour);
+questionList.push(questionFive);
 
-questionTags = ['question1', 'question2', 'question3'];
-answerListTags = ['answerList1', 'answerList2', 'answerList3'];
-inputTags = ['answer1', 'answer2', 'answer3'];
+questionTags = ['question1', 'question2', 'question3', 'question4', 'question5'];
+answerListTags = ['answerList1', 'answerList2', 'answerList3', 'answerList4', 'answerList5'];
+inputTags = ['answer1', 'answer2', 'answer3', 'answer4', 'answer5'];
 
 for (var i = 0; i < questionList.length; i++) {
     document.getElementById(questionTags[i]).innerHTML = questionList[i].questionText;
---
+++ strings.js
@@ -0,0 +1,177 @@
+//document.getElementById("error").innerHTML = "";
+
+//var outputString = "";
+
+var event1 = document.getElementById("button1").addEventListener('click', buttonOutput, false);
+//var event1 = document.getElementById("button1").addEventListener('click', cutString, false);
+//var event2 = document.getElementById("button2").addEventListener('click', resetString, false );
+
+function buttonOutput() {
+        
+    //diffOutput();
+    //searchString();
+    //displayString();
+    //addToString();
+    //replaceString();
+    //replaceLetter();
+    //roundNumber();
+    roundDecimal();
+    
+}
+
+function roundDecimal() {
+    
+    var getNum = parseFloat(document.getElementById("input1").value);
+    var getDecimal = parseInt(document.getElementById("input2").value);
+    
+    //var newNum = Number(Math.round(getNum).toFixed(getDecimal));
+    
+    // Solving decimal of 5 issue: http://www.jacklmoore.com/notes/rounding-in-javascript/
+    var newNum = Number(Math.round(getNum + 'e' + getDecimal) + 'e-' + getDecimal);
+    document.getElementById("result1").innerHTML = "The value: " + getNum + " rounded by " + getDecimal + " decimal places would equal: " + newNum + ".";
+    
+    
+}
+
+function roundNumber() {
+    
+    var getNum = parseFloat(document.getElementById("input1").value);
+    
+    var roundNum = Math.round(getNum);
+    var ceilNum = Math.ceil(getNum);
+    var floorNum = Math.floor(getNum);
+    
+    document.getElementById("result1").innerHTML = "The value: " + getNum + " rounded to the nearest integer would equal: " + roundNum + ".";
+    document.getElementById("result2").innerHTML = "The value: " + getNum + " rounded up to the nearest integer would equal: " + ceilNum + ".";
+    document.getElementById("result3").innerHTML = "The value: " + getNum + " rounded down to the nearest integer would equal: " + floorNum + ".";
+    
+}
+
+function replaceLetter() {
+    
+    var getString = document.getElementById("input1").value;
+    var getChar = document.getElementById("input2").value;
+    var newChar = document.getElementById("input3").value;
+    
+    var stringArray = getString.split("");
+    
+    for (i = 0; i < stringArray.length; i++) {
+        if ((stringArray[i] === getChar) && (getString != "")){
+             stringArray[i] = newChar;
+        }         
+    }
+    
+    var displayString = stringArray.join("");
+    document.getElementById("result1").innerHTML = "String has been changed, it is now: " 
+    document.getElementById("result2").innerHTML = displayString + ".";
+ 
+}
+
+function replaceString() {
+    
+    var getString = document.getElementById("input1").value;
+    var replaceString = document.getElementById("input2").value;
+    var newString = document.getElementById("input3").value;
+    
+    var displayString = getString.replace(replaceString, newString);
+    document.getElementById("result1").innerHTML = "String has been changed, it is now: " 
+    document.getElementById("result2").innerHTML = displayString + ".";
+
+}
+
+function addToString() {
+    
+    var getString = document.getElementById("input1").value;
+    var getChar = document.getElementById("input2").value;
+    var getPos = parseInt(document.getElementById("input3").value);
+    
+    var stringArray = getString.split("");
+    stringArray[getPos - 1] = getChar;
+    
+    var rejoinedString = stringArray.join("");
+    document.getElementById("result1").innerHTML = "Your string is now: " + rejoinedString + ".";
+    
+}
+
+function displayString() {
+
+    var getString = document.getElementById("input1").value;
+    
+    for (i = 0; i < getString.length; i++){
+        outputString += getString.charAt(i) + "<br/>";   
+    }
+    
+    document.getElementById("stringoutput").innerHTML = outputString;
+
+}
+
+function searchString() {
+    
+    var getSentence = document.getElementById("input1").value;
+    var getString = document.getElementById("input2").value;
+    
+    var sentenceArray = getSentence.split();
+    var stringArray = getString.split();
+    
+    var index = 0;
+    
+    //index = getSentence.indexOf(getString);
+    index = getSentence.lastIndexOf(getString);
+    
+    if ((index >= 0) && (getString != "")){
+        //document.getElementById("result1").innerHTML = getString + " found in the sentence, is at position: " + (index + 1) + ".";
+        document.getElementById("result1").innerHTML = "Last occurence of " + getString + " found in the sentence at position: " + (index + 1) + ".";
+    }
+    else {
+        if (getString != "") {
+            document.getElementById("result1").innerHTML = getString + " was not found in the sentence.";
+        }
+    }
+    
+}
+
+function diffOutput() {
+    
+    var getInput = document.getElementById("input1").value;
+    document.getElementById("input2").value = getInput;
+    document.getElementById("input3").value = getInput.toUpperCase();
+    document.getElementById("input4").value = getInput.toLowerCase();
+        
+}
+
+function cutString() {
+    
+    var stringInput = document.getElementById("input1").value;
+    var numberInput = parseInt(document.getElementById("input2").value);
+    
+    var stringChar = stringInput.split("");
+    
+    if ((numberInput >= 0) && (numberInput <= stringChar.length)) {
+    
+        document.getElementById("error").innerHTML = "";
+        var charIndex = stringChar.slice(numberInput -1, numberInput);
+        var remainString = stringChar.slice(numberInput);
+        
+        document.getElementById("stringinput").innerHTML = stringInput;
+        document.getElementById("numberinput").innerHTML = charIndex;
+        document.getElementById("leftover").innerHTML = remainString;
+            
+    }
+    
+    else {
+        
+        document.getElementById("error").innerHTML = "There is no character at that specified position, try again.";
+        
+    }
+    
+}
+
+function resetString() {
+    
+    document.getElementById("input1").value = "";
+    document.getElementById("input2").value = "";
+    document.getElementById("stringinput").innerHTML = "";
+    document.getElementById("numberinput").innerHTML = "";
+    document.getElementById("leftover").innerHTML = "";
+    
+}
