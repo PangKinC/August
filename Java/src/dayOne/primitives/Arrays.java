ab810a6c8644d648228e8c0cfd18b23807e97066
PangKinC <kinchungpang@gmail.com>
2016-08-22 22:22:56 +0100

    blah

--- Java/src/dayOne/primitives/Arrays.java
+++ Java/src/dayOne/primitives/Arrays.java
@@ -6,11 +6,18 @@ package dayOne.primitives;
 public class Arrays {
 
     public static void main(String[] args){
-        int[] test = { 2, 3 };
+        int[] test = { 3, 2, 2, 5, 7, 9, 7, 10};
         int[] testTwo = { 0, 2, 3, 1, 5 };
+        int[] testThree = {1, 0, 2};
 
         //maxEndThree(test);
-        System.out.println(hasTwoThree(test));
+        //countsEven(test);
+        //bigDiff(test);
+        //sumThirteen(test);
+        //sumSixSeven(test);
+        hasTwoTwo(test);
+        //System.out.println(hasTwoThree(test));
+
     }
 
     static void example(){
@@ -75,6 +82,87 @@ public class Arrays {
         return (numbers[0] == 2 || numbers[0] == 3 || numbers[1] == 2 || numbers[1] == 3);
     }
 
+    static void countsEven(int[] numbers){
+        int even = 0;
+        for (int i = 0; i < numbers.length; i++){
+            if (numbers[i] % 2 == 0){
+                even++;
+            }
+        }
+        System.out.println(String.format("Number of evens in the array %s.", even));
+    }
 
+    static void bigDiff(int[] numbers) {
+
+        int smallest = numbers[0];
+        int biggest = numbers[0];
+        int result = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            smallest = Math.min(smallest, numbers[i]);
+            biggest = Math.max(biggest, numbers[i]);
+        }
+
+        result = biggest - smallest;
+
+        System.out.println(String.format("The biggest value is %s, smallest is %s, and the difference is %s.", biggest, smallest, result));
+    }
+
+    static void sumThirteen(int[] numbers) {
+
+        int result = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            result += numbers[i];
+
+            if (numbers[i] == 13){
+                result -= 13;
+                i = numbers.length;
+                System.out.println("13 was found!");
+            }
+            else if (numbers.length == 0){
+                result = 0;
+                System.out.println("Sum is 0, because the array is empty.");
+            }
+        }
+        System.out.println(String.format("The sum of the array is %s.", result));
+    }
+
+    static void sumSixSeven(int[] numbers) {
+        int result = 0;
+        int resultTwo = 0;
+
+        for (int i = 0; i < numbers.length - 1;  i++){
+            if (numbers[i] != 6 && numbers[i] != 7) {
+                result += numbers[i];
+            }
+
+            else if (numbers[i] == 6) {
+                result -= 6;
+            }
+
+            else if (numbers[i] == 7) {
+                    resultTwo = result;
+                    resultTwo += numbers[i];
+            }
+        }
+        System.out.println(resultTwo);
+    }
+
+    static boolean hasTwoTwo(int[] numbers){
+
+        boolean found = false;
+
+        for (int i = 0; i < numbers.length; i++){
+            if (numbers[i] == 2 && numbers[i+1] == 2){
+                System.out.println("Hello");
+                found = true;
+                break;
+            }
+            else { found = false; }
+        }
+        //System.out.println("Test");
+        return found;
+    }
 }
 
