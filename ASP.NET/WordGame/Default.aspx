c731e2c5bb18f6d35b92fe05f8109e29b60ef5a3
PangKinC <kinchungpang@gmail.com>
2016-09-02 00:24:59 +0100

    blah

--- ASP.NET/WordGame/Default.aspx
+++ ASP.NET/WordGame/Default.aspx
@@ -13,28 +13,32 @@
 </head>
 <body style="height: 574px">
     <form id="form1" runat="server">
+    <asp:ScriptManager id="scriptManager" runat="server" />
     <div style="height: 606px">
     
         <asp:Label ID="wordLbl" runat="server" Font-Size="X-Large"></asp:Label>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         <br />
         <br />
-        <asp:Label ID="testLbl" runat="server"></asp:Label>
+        <asp:Label ID="lettersLbl" runat="server"></asp:Label>
         <br />
         <asp:Label ID="testLbl2" runat="server"></asp:Label>
         <br />
         <br />
-        <asp:Button ID="qBtn" runat="server" Text="Q" CommandArgument="Q" OnClick="letterGuessed" />
-        <asp:Button ID="wBtn" runat="server" Text="W" CommandArgument="W" OnClick="letterGuessed" />
-        <asp:Button ID="eBtn" runat="server" Text="E" CommandArgument="E" OnClick="letterGuessed" />
-        <asp:Button ID="rBtn" runat="server" Text="R" CommandArgument="R" OnClick="letterGuessed" />
-        <asp:Button ID="tBtn" runat="server" Text="T" CommandArgument="T" OnClick="letterGuessed" />
-        <asp:Button ID="yBtn" runat="server" Text="Y" CommandArgument="Y" OnClick="letterGuessed" />
-        <asp:Button ID="uBtn" runat="server" Text="U" CommandArgument="U" OnClick="letterGuessed" />
-        <asp:Button ID="iBtn" runat="server" Text="I" CommandArgument="I" OnClick="letterGuessed" />
-        <asp:Button ID="oBtn" runat="server" Text="O" CommandArgument="O" OnClick="letterGuessed" />
-        <asp:Button ID="pBtn" runat="server" Text="P" CommandArgument="P" OnClick="letterGuessed" />
+        <asp:Button ID="qBtn" runat="server" Text="Q" CommandArgument="Q" OnCommand="letterGuessed" />
+        <asp:Button ID="wBtn" runat="server" Text="W" CommandArgument="W" OnCommand="letterGuessed" />
+        <asp:Button ID="eBtn" runat="server" Text="E" CommandArgument="E" OnCommand="letterGuessed" />
+        <asp:Button ID="rBtn" runat="server" Text="R" CommandArgument="R" OnCommand="letterGuessed" />
+        <asp:Button ID="tBtn" runat="server" Text="T" CommandArgument="T" OnCommand="letterGuessed" />
+        <asp:Button ID="yBtn" runat="server" Text="Y" CommandArgument="Y" OnCommand="letterGuessed" />
+        <asp:Button ID="uBtn" runat="server" Text="U" CommandArgument="U" OnCommand="letterGuessed" />
+        <asp:Button ID="iBtn" runat="server" Text="I" CommandArgument="I" OnCommand="letterGuessed" />
+        <asp:Button ID="oBtn" runat="server" Text="O" CommandArgument="O" OnCommand="letterGuessed" />
+        <asp:Button ID="pBtn" runat="server" Text="P" CommandArgument="P" OnCommand="letterGuessed" />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="scoreLbl" runat="server"></asp:Label>
         <br />
-        <asp:Button ID="aBtn" runat="server" Text="A" CommandArgument="A" OnClick="letterGuessed" />
+        <asp:Button ID="aBtn" runat="server" Text="A" CommandArgument="A" OnCommand="letterGuessed" />
         <asp:Button ID="sBtn" runat="server" Text="S" CommandArgument="S" OnCommand="letterGuessed" />
         <asp:Button ID="dBtn" runat="server" Text="D" CommandArgument="D" OnCommand="letterGuessed" />
         <asp:Button ID="fBtn" runat="server" Text="F" CommandArgument="F" OnCommand="letterGuessed" />
@@ -43,6 +47,9 @@
         <asp:Button ID="jBtn" runat="server" Text="J" CommandArgument="J" OnCommand="letterGuessed" />
         <asp:Button ID="kBtn" runat="server" Text="K" CommandArgument="K" OnCommand="letterGuessed" />
         <asp:Button ID="lBtn" runat="server" Text="L" CommandArgument="L" OnCommand="letterGuessed" />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="noGuessLbl" runat="server"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         <br />
         <asp:Button ID="zBtn" runat="server" Text="Z" CommandArgument="Z" OnCommand="letterGuessed" />
         <asp:Button ID="xBtn" runat="server" Text="X" CommandArgument="X" OnCommand="letterGuessed" />
@@ -51,8 +58,22 @@
         <asp:Button ID="bBtn" runat="server" Text="B" CommandArgument="B" OnCommand="letterGuessed" />
         <asp:Button ID="nBtn" runat="server" Text="N" CommandArgument="N" OnCommand="letterGuessed" />
         <asp:Button ID="mBtn" runat="server" Text="M" CommandArgument="M" OnCommand="letterGuessed" />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="multiLbl" runat="server"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         <br />
         <br />
+        <asp:Timer ID="timer" runat="server" Interval="1000" OnTick="timer_Tick"> </asp:Timer>
+        <asp:UpdatePanel runat="server" id="timePanel" updatemode="Conditional">
+        <Triggers>
+            <asp:AsyncPostBackTrigger controlid="timer" EventName="Tick" />
+        </Triggers>
+            <ContentTemplate>
+                &nbsp;
+                <asp:Label ID="timeLbl" runat="server"></asp:Label>
+            </ContentTemplate>
+        </asp:UpdatePanel>
+ 
         <br />
     
     </div>
--- ASP.NET/WordGame/Default.aspx.cs
+++ ASP.NET/WordGame/Default.aspx.cs
@@ -6,81 +6,183 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+// To-do List:
+// Add timer to the game, decrement/increment depending if user guesses word correctly.
+// Make the game detect keypresses from keyboard as a second input method.
+// Create a hint for each of the hidden word possibly using the split() method.
+// Let the hidden word ignore spaces so can use multiple words.
+// Use Bootstrap & CSS to make things more pretty.
+
 public partial class _Default : System.Web.UI.Page
 {
-    private List<Button> btnList;
-    private String[] wordsList;
-    private String[] readFile;
-    private String word;
-    private String hidden;
-    private int randomIndex;
+    private static List<Button> btnList;
+    private static string[] wordsList;
+    private static string[] readFile;
+    private static char[] hiddenChar;
+    private static char[] wordChar;
+
+    private static string path;
+    private static string word;
+    private static string hidden;
+    private static string letter;
+
+    private static int randomIndex;
+    private static int wrongGuess;
+    private static int timeLeft;
+
+    private static double score;
+    private static double currentScore;
+    private static double multiplier;
+
+    private static bool ansCorrect;
 
     protected void Page_Load(object sender, EventArgs e)
     {
         btnList = new List<Button>() { qBtn, wBtn, eBtn, rBtn, tBtn, yBtn, uBtn, iBtn, oBtn, pBtn,
                                        aBtn, sBtn, dBtn, fBtn, gBtn, hBtn, jBtn, kBtn, lBtn,
                                        zBtn, xBtn, cBtn, vBtn, bBtn, nBtn, mBtn };
-        if (!Page.IsPostBack)
+
+
+        if (!scriptManager.IsInAsyncPostBack)
         {
-            setup();
+            Session["timeout"] = DateTime.Now.AddSeconds(30).ToString();
         }
+
+        if (!IsPostBack)
+        {
+            scoreLbl.Text = "SCORE: 0";
+            setup();
+        }   
     }
 
-    public void setup()
+    protected void setup()
     {
+        hidden = "";
+        lettersLbl.Text = "";
         wordLbl.Text = "";
-        // readFile = File.ReadAllLines(@"C:\Users\Suijin\Documents\GitHub\August\ASP.NET\WordGame\Resources\words.txt");
-        readFile = File.ReadAllLines(@"C:\Users\student\Kin Pang\GitHub\August\ASP.NET\WordGame\Resources\wordsP.txt");
+
+        wrongGuess = 0;
+        multiplier = 2.00;
+        score = 500;
+        timeLeft = 10;
+
+        ansCorrect = false;
+
+        noGuessLbl.Text = "Wrong Letter Count: " + wrongGuess;
+        multiLbl.Text = "Current Multiplier: x" + multiplier;
+
+        path = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, @"Resources\wordsP.txt");
+        readFile = File.ReadAllLines(path);
         randomIndex = (new Random().Next(readFile.Length));
         word = readFile[randomIndex].ToUpper();
+
+        testLbl2.Text = word;
+
+        for (int i = 0; i < word.Length; i++) {
+            hidden += "_";
+        }
+
+        for (int i = 0; i < hidden.Length; i++) {
+            wordLbl.Text += hidden.Substring(i, 1);
+            wordLbl.Text += " ";
+        }
 
         foreach (Button b in btnList) {
             b.Enabled = true;
         }
-
-        for (int i = 0; i < word.Length; i++) {
-            hidden += " _ ";
-        }
-
-        wordLbl.Text = hidden;
-        testLbl2.Text = word;
     }
 
-    protected void letterGuessed (object sender, EventArgs e)
+    public void letterGuessed (object sender, CommandEventArgs e)
     {
-        Button btn = sender as Button;
-        char letter = btn.Text.ElementAt(0);
-
-        string test = word;
-
-        //string letter = e.CommandArgument.ToString();
-        //letter.ToCharArray();
+        letter = e.CommandArgument.ToString();
+        letter.ToCharArray();
 
         if (word.Contains(letter)) {
-            char[] hiddenChar = hidden.ToCharArray();
-            char[] wordChar = word.ToCharArray();
-
- 
+            hiddenChar = hidden.ToCharArray();
+            wordChar = word.ToCharArray();
 
             for (int i = 0; i < wordChar.Length; i++) {
-                if (wordChar[i] == letter) { hiddenChar[i] = letter; }
+                if (wordChar[i] == letter[0]) { hiddenChar[i] = letter[0]; }
             }
-
+       
             hidden = new string(hiddenChar);
             updateScreen();
         }
 
-        testLbl.Text += btn.Text;
+        else {
+            lettersLbl.Text += e.CommandArgument;
+            wrongGuess++;
+        
+            if (wrongGuess >= 1 && wrongGuess < 2) { multiplier = 1.75; }
 
-        foreach (Button b in btnList) {
-            if (b.Text.Equals(btn.Text)) { b.Enabled = false; }
+            else if (wrongGuess >= 2 && wrongGuess < 4 ) { multiplier = 1.50; }
+
+            else if (wrongGuess >= 4 && wrongGuess < 6) { multiplier = 1.25; }
+
+            else { multiplier = 1.00; }
+
+            updateScreen();
+        }
+
+        foreach (Button b in btnList)
+        {
+            if (b.Text.Equals(e.CommandArgument)) { b.Enabled = false; }
+        }
+
+        if (hidden == word) {
+            score *= multiplier;
+            ansCorrect = true;
+            updateScreen();
+            setup();
         }
 
     }
 
     protected void updateScreen()
     {
-        wordLbl.Text = hidden;
+        wordLbl.Text = "";
+ 
+        if (ansCorrect == true) {
+            currentScore += score;
+            scoreLbl.Text = "SCORE: " + currentScore;
+        }
+
+        noGuessLbl.Text = "Wrong Letter Count: " + wrongGuess;
+        multiLbl.Text = "Current Multiplier: x" + multiplier;
+
+        for (int i = 0; i < hidden.Length; i++) {
+            wordLbl.Text += hidden.Substring(i, 1);
+            wordLbl.Text += " ";
+        }
     }
 
+    protected void timer_Tick(object sender, EventArgs e)
+    {
+        /*if (timeLeft > 0)
+        {
+            timeLeft--;
+            timeLbl.Text = "Time Remaining: " + timeLeft;
+        }
+
+        else
+        {
+            foreach (Button b in btnList)
+            {
+                b.Enabled = false;
+            }
+
+            timeLbl.Text = "Time's Up!";
+
+            wordLbl.Text = word;
+
+        
+        }*/
+        if (0 > DateTime.Compare(DateTime.Now, DateTime.Parse(Session["timeout"].ToString())))
+        {
+            timeLbl.Text = "Number of seconds left: " +
+            ((Int32)DateTime.Parse(Session["timeout"].
+            ToString()).Subtract(DateTime.Now).TotalSeconds).ToString();
+        }
+
+    }
 }
