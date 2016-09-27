04f34f6a1849bd9b7ef8c266d6f561825ee63be7
PangKinC <kinchungpang@gmail.com>
2016-09-28 00:29:04 +0100

    blah

---
+++ ASP.NET/Ratings/Ratings/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Ratings.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ratings.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AcceptVerbs("post")]
+        public ActionResult Rate(FormCollection form)
+        {
+            var rate = Convert.ToInt32(form["Score"]);
+            var id = Convert.ToInt32(form["ArticleID"]);
+            if (Request.Cookies["rating" + id] != null)
+                return Content("false");
+            Response.Cookies["rating" + id].Value = DateTime.Now.ToString();
+            Response.Cookies["rating" + id].Expires = DateTime.Now.AddYears(1);
+            ArticleRating ar = repository.IncrementArticleRating(rate, id);
+            return Json(ar);
+        }
+
+
+    }
+
+
+}
---
+++ ASP.NET/Ratings/Ratings/Models/ArticleRating.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ratings.Models
+{
+    public class ArticleRating : Rating
+    {
+        public int ArticleID { get; set; }
+        public int Rating { get; set; }
+        public int TotalRaters { get; set; }
+        public double AverageRating { get; set; }
+
+        public string Check(double lower, double upper, double toCheck)
+        {
+            return toCheck > lower && toCheck <= upper ? " checked=\"checked\"" : null;
+        }
+
+        public ArticleRating IncrementArticleRating(int rate, int id)
+        {
+            var article = de.ArticleSet
+                      .Where(a => a.ArticleID == id)
+                      .First();
+            article.Rating += rate;
+            article.TotalRaters += 1;
+            de.SaveChanges();
+            var ar = new ArticleRating()
+            {
+                ArticleID = this.ArticleID,
+                Rating =.Rating,
+                TotalRaters = article.TotalRaters,
+                AverageRating = Convert.ToDouble(article.Rating) / Convert.ToDouble(article.TotalRaters)
+            };
+            return ar;
+        }
+    }
+
+
+
+
+}
